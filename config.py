bc9ed2e1a9934871cf4575043181d3fb780bffc4
chuchu <xriseugene@gmail.com>
2017-02-14 12:02:24 +0000

    Add specific environment configuration details

--- config.py
+++ config.py
@@ -1,2 +1,28 @@
+# config.py
 # File to store our config details such as  Secret Key and Firebase Details
 # Nothing yet :(
+class Config(object):
+    """
+    Common configs
+    """
+class DevelopmentConfig(Config):
+    """
+    Development Configs
+    """
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Prodcution configs
+    """
+
+    DEBUG = False
+
+app_config = {
+        'development': DevelopmentConfig,
+        'production': ProductionConfig
+}
+
+
+
