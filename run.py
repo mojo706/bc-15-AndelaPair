5613c242d7d574ecb9272feabc57d2e80416aede
chuchu <xriseugene@gmail.com>
2017-02-14 14:06:26 +0000

    Add runtime configurations

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 # run.py
 # configs to run our app
 
-from app import app
+import os
+from app import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
