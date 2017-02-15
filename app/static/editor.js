f1359d5e02f48fad348c80bdce9992f7fe8883a0
chuchu <xriseugene@gmail.com>
2017-02-15 09:09:04 +0000

    Add the code that creates Ace editor on the main page

---
+++ app/static/editor.js
@@ -0,0 +1,45 @@
+// app/static/editor.js
+// This is the code that loads the editor. Using Ace instead of Codemirror
+
+
+function init() {
+      //// Initialize Firebase.
+      var config = {
+          apiKey: "AIzaSyAcGzXdxjWp7AuQySWpqF0ZA7dewKheo7s",
+          authDomain: "andelapair.firebaseapp.com",
+          databaseURL: "https://andelapair.firebaseio.com",
+      };
+      firebase.initializeApp(config);
+
+      //// Get Firebase Database reference.
+      var firepadRef = getExampleRef();
+
+      //// Create ACE
+      var editor = ace.edit("firepad-container");
+      editor.setTheme("ace/theme/monokai");
+      var session = editor.getSession();
+      session.setUseWrapMode(true);
+      session.setUseWorker(false);
+      session.setMode("ace/mode/python");
+
+      //// Create Firepad.
+      var firepad = Firepad.fromACE(firepadRef, editor, {
+        defaultText: '# Start Coding Something In Python! }'
+      });
+    }
+
+    // Helper to get hash from end of URL or generate a random one.
+    function getExampleRef() {
+      var ref = firebase.database().ref();
+      var hash = window.location.hash.replace(/#/g, '');
+      if (hash) {
+        ref = ref.child(hash);
+      } else {
+        ref = ref.push(); // generate unique location.
+        window.location = window.location + '#' + ref.key; // add it as a hash to the URL.
+      }
+      if (typeof console !== 'undefined') {
+        console.log('Firebase data: ', ref.toString());
+      }
+      return ref;
+    }
